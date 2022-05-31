a62a461983486da29917e098b522d425d06e714c
Nikita <72461796+Gandalf329@users.noreply.github.com>
2022-05-31 20:44:26 +0300

    Update MergeSort.cs

--- sort/MergeSort.cs
+++ sort/MergeSort.cs
@@ -7,10 +7,14 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            int[] array = new int[6] { 7, 3, 8, 2, 1, 4 };
+            array = Sort(array);
 
-
+            foreach(int num in array)
+            {
+                Console.WriteLine(num);
+            }
         }
-
         public static int[] Sort(int[] array)
         {
             if (array.Length <= 1)
@@ -26,27 +30,54 @@ namespace Algorithms
                 {
                     left[i] = array[i];
                 }
-                for (int i = 0; right.Length > i; i++)
+                for (int i = 0;i < right.Length ; i++)
                 {
                     right[i] = array[i + left.Length];
                 }
 
-                if (left.Length < 1)
+                if (left.Length > 1)
                 {
                     left = Sort(left);
                 }
-                if (right.Length < 1)
+                if (right.Length > 1)
                 {
                     right = Sort(right);
                 }
                 array = MergeSort(left, right);
+
             }
+
             return array;
         }
 
-        public static int[] MergeSort(int[] left, int[] right)
+        static int[] MergeSort(int[] left, int[] right)
         {
+            int[] array = new int[left.Length + right.Length];
+              
+            int l = 0;  
+            int r = 0;  
 
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (r >= right.Length)
+                {
+                    array[i] = left[l];
+                    l++;
+                }
+                else if (l < left.Length && left[l] < right[r])
+                {
+                    array[i] = left[l];
+                    l++;
+                }
+                else
+                {
+                    array[i] = right[r];
+                    r++;
+                   
+                }
+            }
+            return array;
         }
     }
-   
+}
